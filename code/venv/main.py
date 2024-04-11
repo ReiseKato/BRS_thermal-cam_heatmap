a7cc00517086a9892b24b41015844aadda289af7
Reise Thomas Kato <132662297+ReiseKato@users.noreply.github.com>
2024-04-11 15:49:56 +0200

    added main

--- code/venv/main.py
+++ code/venv/main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation
 import numpy as np
 import time
 
@@ -10,7 +11,7 @@ def show_heatmap_single_frame(data: np.array, size: (int, int), vmin: float, vma
     adj_data = np.reshape(data, size)
     plt.imshow(adj_data, vmin=vmin, vmax=vmax, cmap='inferno', interpolation='hamming')
     plt.colorbar()
-    #plt.show()
+    plt.show()
 
 def show_heatmap_video(data: np.array, size: (int, int), vmin: float, vmax: float, frequency: float):
     n = len(data)
@@ -26,12 +27,32 @@ def show_heatmap_video(data: np.array, size: (int, int), vmin: float, vmax: floa
         show_heatmap_single_frame(cut_data, size=size, vmin=vmin, vmax=vmax)
         time.sleep(1.0/frequency)
 
+def main():
+    nx = 50
+    ny = 50
+
+    fig = plt.figure()
+    data = np.random.rand(nx, ny)
+    im = plt.imshow(data)
+
+    def init():
+        im.set_data(np.zeros((nx, ny)))
+
+    def animate(i):
+        # xi = i // ny
+        # yi = i % ny
+        data = np.random.rand(nx, ny)
+        im.set_data(data)
+        return im
+
+    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=50, repeat=False)
 
 
 if __name__ == "__main__":
     size = (32, 24)
     data = np.random.randint(0, 100, size=32*24*16*10)
-    show_heatmap_video(data, size=size, vmin=0, vmax=100, frequency=16.0)
+    # show_heatmap_video(data, size=size, vmin=0, vmax=100, frequency=16.0)
+    main()
 
 
 
